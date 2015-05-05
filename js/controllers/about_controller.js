c2a4131bc8ed945462b5ae65a2379eca7652b1c7
BrianCaul <caul1990@hotmail.com>
2015-05-05 14:09:49 +0100

    Style changes

--- js/controllers/about_controller.js
+++ js/controllers/about_controller.js
@@ -5,12 +5,12 @@ define(['js/app'], function (app) {
     var injectParams = ['$scope'];
 
     var about_controller = function ($scope) {
- 
-       
-  
-	};
+    
+        
+    };
+
     about_controller.$inject = injectParams;
 
-    app.controller('about_controller', about_controller);
+    app.register.controller('about_controller', about_controller);
 
 });
